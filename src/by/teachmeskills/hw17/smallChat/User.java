9b9e7a3a08835bf64da0ac39312aa1c315dde16b
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-03-20 23:01:00 +0300

    Changed method equals in User class

--- src/by/teachmeskills/hw17/smallChat/User.java
+++ src/by/teachmeskills/hw17/smallChat/User.java
@@ -17,6 +17,7 @@ public class User {
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
 
         User user = (User) obj;
         return this.nickname.equals(user.nickname);
